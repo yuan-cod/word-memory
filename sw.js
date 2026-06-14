e5b290101689ffa1766bc13583db4c604bfddad9
考研单词记忆 <wordmemory@local>
2026-06-14 17:02:49 +0800

    fix: service worker use Promise.allSettled for resilient caching

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
 // 考研单词记忆 - Service Worker
-const CACHE = 'wordmemory-v1';
+const CACHE = 'wordmemory-v2';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      Promise.allSettled(FILES.map(f => cache.add(f).catch(err => {
+        console.warn('SW: cache add failed for', f, err);
+      })))
+    )
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
